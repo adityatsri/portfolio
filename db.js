07d6d3c5fd362bdfac08367febb5f3fef64a755d
sujancho <jujjavarapu.sujan_chowdary@nokia.com>
2026-05-05 14:56:00 +0000

    fix pg ssl config for Neon/Vercel

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
+  max: 1,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
